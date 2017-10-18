4616e59562e680c1a19e8649fb4bccb5306e3c8b
Bastin Robins J <bastinrobins@gmail.com>
2017-10-18 11:33:52 +0530

    Added templates directory

--- main.py
+++ main.py
@@ -7,27 +7,41 @@ import tornado.web
 class MainHandler(tornado.web.RequestHandler):
 
     def get(self):
-        self.write("Hello World")
+        # self.write("Hello World")
+        self.render('index.html')
 
 
 class AboutHandler(tornado.web.RequestHandler):
 
     def get(self):
-        self.write('About us')
+        self.render('about.html')
 
 
 class ContactHandler(tornado.web.RequestHandler):
 
     def get(self):
-        self.write("Contact us")
+        self.render('contact.html')
 
 
 def main():
+
+    settings = dict(
+        cookie_secret=str(os.urandom(45)),
+        template_path=os.path.join(os.path.dirname(__file__), "templates"),
+        static_path=os.path.join(os.path.dirname(__file__), "static"),
+        xsrf_cookies=True,
+        autoreload=True,
+        gzip=True,
+        debug=True,
+        login_url='/login',
+        autoescape=None
+    )
+
     application = tornado.web.Application([
         (r"/", MainHandler),
         (r"/about", AboutHandler),
         (r"/contact", ContactHandler)
-    ])
+    ], **settings)
 
     http_server = tornado.httpserver.HTTPServer(application)
     port = int(os.environ.get("PORT", 5000))
